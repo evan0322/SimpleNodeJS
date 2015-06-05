d12792d2e92d69aaf77259cf580ec68def43cd24
Wei Xie <weixie@weis-mac-mini.dev.ad.entrust.com>
2015-06-05 11:16:08 -0400

    Add json response

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
+var json = {
+            "Name":"Entrust",
+            "Group":"DataCaed",
+            "Address":{
+                        "Country":"Canada",
+                        "Province":"Ontario",
+                        "City":"Ottawa"
+                        }
+            };
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.json(json);
 });
 
 app.listen(app.get('port'), function() {
